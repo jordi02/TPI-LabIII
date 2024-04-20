ae10aac965d992bef08e22f3451c59e1c38c8495
Jordi Gimbernat <jordigimbernat02@gmail.com>
2024-04-20 15:30:29 -0300

    primer commit

---
+++ Trabajo-Practico/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar/Navbar'
+function App() {
+  return (<>
+      <NavBar/>
+
+  </>
+  )
+}
+
+export default App
---
+++ Trabajo-Practico/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    return (
+        <>
+            <footer class="py-3 bg-dark  text-white">
+    <ul class="nav justify-content-center  pb-3 mb-3">
+      <li class="nav-item"><a href="https://www.facebook.com/jordi.gimbernat" target="_blank" class="nav-link px-2 text-muted">Facebook</a></li>
+      <li class="nav-item"><a href="https://twitter.com/Jordi_Gimbernat" target="_blank" class="nav-link px-2 text-muted">Twitter</a></li>
+      <li class="nav-item"><a href="https://www.instagram.com/jordi_gimbernat/" target="_blank" class="nav-link px-2 text-muted">Instagram</a></li>
+      
+    </ul>
+    <p class="text-center text-muted">© 2024 Company, Inc</p>
+  </footer>
+        </>
+    );
+};
+export default Footer;
