081870216fc0f578d6182f437693c1b052b06585
federodriguez33 <federicosebastianrodriguez@gmail.com>
2024-06-21 18:03:07 -0300

    7.9.1

--- Trabajo-Practico/src/components/Footer/Footer.jsx
+++ Trabajo-Practico/src/components/Footer/Footer.jsx
@@ -1,21 +1,47 @@
-//import "./Footer.css";
+import "./Footer.css";
 
 const Footer = () => {
 
   return (
 
-    <footer className="py-3 bg-dark text-white fixed-bottom" style={{ marginTop: "100px" }}>
-      <div className="container">
-        <ul className="nav justify-content-center pb-3 mb-3">
-          <li className="nav-item"><a href="" target="_blank" rel="noopener noreferrer" className="nav-link px-2 text-white">Facebook</a></li>
-          <li className="nav-item"><a href="" target="_blank" rel="noopener noreferrer" className="nav-link px-2 text-white">Twitter</a></li>
-          <li className="nav-item"><a href="" target="_blank" rel="noopener noreferrer" className="nav-link px-2 text-white">Instagram</a></li>
-        </ul>
-        <p className="text-center text-white">© 2024 PUROHABITO, Nutricion</p>
+    <>
+      <div className="Footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-lg-5 col-12 ft-1">
+    
+              <p>© 2024 PUROHABITO, Nutricion</p>
+              
+            </div>
+            <div className="col-md-6 col-lg-4 col-12 ft-3">
+              <h5>Contactanos</h5>
+              <div className="footer-icons">
+                <i className="fa-brands fa-facebook"></i>
+                <i className="fa-brands fa-twitter"></i>
+                <i className="fa-brands fa-instagram"></i>
+                <i className="fa-brands fa-linkedin-in"></i>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </footer>
+      <div className='Last-footer'>
+        <p>Design By Waleed Ishfaq</p>
+      </div>
+    </>
 
   );
+
+  // <footer className="py-3 bg-dark text-white fixed-bottom" style={{ marginTop: "100px" }}>
+  //   <div className="container">
+  //     <ul className="nav justify-content-center pb-3 mb-3">
+  //       <li className="nav-item"><a href="" target="_blank" rel="noopener noreferrer" className="nav-link px-2 text-white">Facebook</a></li>
+  //       <li className="nav-item"><a href="" target="_blank" rel="noopener noreferrer" className="nav-link px-2 text-white">Twitter</a></li>
+  //       <li className="nav-item"><a href="" target="_blank" rel="noopener noreferrer" className="nav-link px-2 text-white">Instagram</a></li>
+  //     </ul>
+  //     <p className="text-center text-white">© 2024 PUROHABITO, Nutricion</p>
+  //   </div>
+  // </footer>
 
 };
 
