1b2599f2e7a3c256169ad715d8b2e4ce43443467
federodriguez33 <federicosebastianrodriguez@gmail.com>
2024-06-26 20:04:43 -0300

    8.7

--- Trabajo-Practico/src/components/itemList/ItemListContainer.jsx
+++ Trabajo-Practico/src/components/itemList/ItemListContainer.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import Item from "../item/Item";
-//import Uno from "../../assets/itemListContainer/1.jpg";
-import Dos from "../../assets/itemListContainer/2.jpg";
-import Tres from "../../assets/itemListContainer/3.jpg";
-import Cuatro from "../../assets/itemListContainer/4.jpg";
 import Cinco from "../../assets/itemListContainer/5.jpg";
+import Seis from "../../assets/itemListContainer/6.jpeg";
+import Siete from "../../assets/itemListContainer/7.jpg";
+import Ocho from "../../assets/itemListContainer/8.jpg";
 import './ItemListContainer.css';
 //Modulos Firebase
 import { getFirestore, collection, getDocs } from "firebase/firestore";
@@ -36,31 +35,51 @@ const ItemListContainer = () => {
 
     <>
 
-      <div className="container-fluid">
-        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={Cinco} className="d-block w-100 carousel-image" alt="Uno" />
-            </div>
-            <div className="carousel-item">
-              <img src={Dos} className="d-block w-100 carousel-image" alt="Dos" />
-            </div>
-            <div className="carousel-item">
-              <img src={Tres} className="d-block w-100 carousel-image" alt="Tres" />
-            </div>
-            <div className="carousel-item">
-              <img src={Cuatro} className="d-block w-100 carousel-image" alt="Cuatro" />
+      <div id="carouselExampleCaptions" className="carousel slide">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div className="carousel-inner ">
+          <div className="carousel-item active">
+            <img src={Cinco} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", padding: "10px", borderRadius: "5px", margin: "0 auto", maxWidth: "50%"}}>
+              <h5>Consulta nutricional</h5>
+              <p>Ofrecemos consultas online y presenciales para tu comodidad y conveniencia.</p>
             </div>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+          <div className="carousel-item">
+            <img src={Seis} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", padding: "10px", borderRadius: "5px", margin: "0 auto", maxWidth: "50%"}}>
+              <h5>Libro disponible</h5>
+              <p>Adquirilo en nuestra tienda y obtené descuentos exclusivos</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={Siete} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", padding: "10px", borderRadius: "5px", margin: "0 auto", maxWidth: "50%"}}>
+              <h5>Master en nutricion deportiva</h5>
+              <p>Junto a la UCAM y al ICNS, 2 años de pura capacitación</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={Ocho} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", padding: "10px", borderRadius: "5px", margin: "0 auto", maxWidth: "50%"}}>
+              <h5>Nuevos productos en nuestros catalogos</h5>
+              <p>HXNutrition nos acercan Fit Food Gourmet, para llenarte de energia</p>
+            </div>
+          </div>
         </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
 
       <div className="mt-5">
